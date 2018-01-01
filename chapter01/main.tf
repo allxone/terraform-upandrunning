c44dca599a57f37b95d21ee20dd3d84e23ebe55e
allxone <allxone@hotmail.com>
2018-01-01 22:20:06 +0100

    AWS security group, graph, input/output variables

--- chapter01/main.tf
+++ chapter01/main.tf
@@ -1,3 +1,7 @@
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+}
+
 provider "aws" {
     region = "us-east-1"
 }
@@ -5,8 +9,26 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-40d28157"
     instance_type = "t2.micro" 
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
 
     tags {
         Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
     }
 }
